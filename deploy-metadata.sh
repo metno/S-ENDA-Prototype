df3375fc2a8b2164958a8578f9b9628f03e9cb6d
Martin Grønlien Pejcoch <martinp@met.no>
2020-05-11 20:56:45 +0200

    Update hook

--- deploy-metadata.sh
+++ deploy-metadata.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+echo "Webhook triggered." | systemd-cat -t webhook-handler
 
 # Work in shared folder
 cd /vagrant
 
 # Check out latest version of metadata
-git clone https://github.com/metno/S-ENDA-metadata
+if [ -d S-ENDA-metadata ]; then
+  echo "Repository exists locally, running git pull." | systemd-cat -t webhook-handler
+  git pull
+fi
+  echo "Cloning repository." | systemd-cat -t webhook-handler
+  git clone https://github.com/metno/S-ENDA-metadata
+fi
+
 
 # Restart catalog-service-api
 docker-compose restart catalog-service-api
