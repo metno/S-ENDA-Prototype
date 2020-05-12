4d3b1dc6bec43f3ecf9ea9a339e9b42200b605e9
Martin Grønlien Pejcoch <martinp@met.no>
2020-05-12 22:15:33 +0200

    Fix deploy script

--- deploy-metadata.sh
+++ deploy-metadata.sh
@@ -10,12 +10,12 @@ mkdir -p isostore
 if [ -d S-ENDA-metadata ]; then
   echo "Repository exists locally, running git pull." | systemd-cat -t webhook-handler
   git pull
-fi
+else
   echo "Cloning repository." | systemd-cat -t webhook-handler
   git clone https://github.com/metno/S-ENDA-metadata
 fi
 
-docker-compose run -v ./isostore:/isostore -v ./S-ENDA-metadata:/mcfdir iso-converter convert-all-mcfs.py --mcfdir /mcfdir --outdir /isostore
+docker-compose run -v /vagrant/isostore:/isostore -v /vagrant/S-ENDA-metadata:/mcfdir iso-converter convert-all-mcfs.py --mcfdir /mcfdir --outdir /isostore
 
 # Restart catalog-service-api
 docker-compose restart catalog-service-api
